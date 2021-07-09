b61df05fca5b941551409c825a16d3fdf050db45
J-nice Olisa <lunakoj@gmail.com>
2021-07-09 01:07:23 +0100

    Updated vies to show all airports initially and show all aircraft using custom images

--- map_display/views.py
+++ map_display/views.py
@@ -7,16 +7,24 @@ import airportsdata
 
 
 airports = airportsdata.load('IATA')  # key is IATA code
-
+airports_b = airportsdata.load()  # key is ICAO code
+ng_airports_icao = ["DN50", "DN51", "DN53", "DN54", "DN55", "DN56", "DNAA", "DNAK", "DNAS",
+ "DNBE", "DNBI", "DNCA", "DNEN", "DNGO", "DNIB", "DNIL", "DNIM", "DNJO", "DNKA", "DNKN", "DNMA", "DNMK", 
+"DNMM", "DNMN", "DNOS", "DNPO", "DNSO", "DNTZ", "DNYO", "DNZA"]
 
 # Create your views here.
 
 def display(request):
-    # obj  = get_object_or_404(MapDisplay, id= 1)
     # Initiate Folium
     m = folium.Map(width=800, height=500, location= (airports['ABV']['lat'],airports['ABV']['lon']), zoom_start=6.5)
+
     # Location maker
     folium.Marker((airports['ABV']['lat'],airports['ABV']['lon']), tooltip='click for more', popup= airports['ABV']['name'],).add_to(m)
+    fg = folium.FeatureGroup(name="My Map")
+    for _ in ng_airports_icao:
+        fg.add_child(folium.Marker((airports_b[_]['lat'],airports_b[_]['lon']), tooltip=airports_b[_]['iata'], popup= airports_b[_]['name'],icon = folium.Icon(icon='plane', prefix='fa')))
+    m.add_child(fg)
+
     form = Display_form(request.POST or None)
     e = ''
 
@@ -28,14 +36,14 @@ def display(request):
             print(flights)
             # Initiate Folium
             m = folium.Map(width=800, height=500, location= (airports['ABV']['lat'],airports['ABV']['lon']), zoom_start=6.5)
+            fg = folium.FeatureGroup(name="My Map")
 
             for data in range(len(flights)):
+                # Output MapDisplay objects from the database
                 MapDisplay.objects.get_or_create(airline_icao = form.cleaned_data.get('airline_icao'), aircraft_reg = flights[data].registration, origin_airport = flights[data].origin_airport_iata, destination_airport = flights[data].destination_airport_iata, long = flights[data].longitude, lat = flights[data].latitude)[0]
-                # Location maker
-                folium.Marker((flights[data].latitude,flights[data].longitude), tooltip= flights[data].registration, popup= get_object_or_404(MapDisplay, aircraft_reg = flights[data].registration), 
-                                icon = folium.Icon(color='red', icon='plane', prefix='fa')).add_to(m)
-                # # fetch flight info object from database
-                # flight_info_list = MapDisplay.objects.order_by('created')
+                # flight position  maker
+                fg.add_child(folium.Marker((flights[data].latitude,flights[data].longitude), tooltip= flights[data].registration, popup= get_object_or_404(MapDisplay, aircraft_reg = flights[data].registration), icon = folium.features.CustomIcon('./static/images/plane11.png', icon_size = (25,25))))
+            m.add_child(fg)
         else:
             e = "No flight for Airline at the moment"
     else:
